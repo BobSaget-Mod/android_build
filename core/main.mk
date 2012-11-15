2b38a1b2eef01e8e928f440e56c6b1866cb5c8c6
rascarlo <rascarlo@gmail.com>
2012-11-15 12:51:17 +0100

    Squash commits of @rascarlo
    add support for overriding build.prop settings
    
    comment out the build date assert
    
    Debugging TRUE
    
    Dexpreopt FALSE
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -304,7 +304,7 @@ ifneq (,$(user_variant))
     ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.lockprof.threshold=500
   else
     # Disable debugging in plain user builds.
-    enable_target_debugging :=
+    enable_target_debugging := true
   endif
 
   # Turn on Dalvik preoptimization for user builds, but only if not
@@ -313,7 +313,7 @@ ifneq (,$(user_variant))
   ifneq (true,$(DISABLE_DEXPREOPT))
     ifeq ($(user_variant),user)
       ifeq ($(HOST_OS),linux)
-        WITH_DEXPREOPT := true
+        WITH_DEXPREOPT := false
       endif
     endif
   endif
@@ -344,6 +344,7 @@ endif # !enable_target_debugging
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 tags_to_install := debug eng
+WITH_DEXPREOPT := false
 ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))),)
   # Don't require the setup wizard on eng builds
   ADDITIONAL_BUILD_PROPERTIES := $(filter-out ro.setupwizard.mode=%,\
