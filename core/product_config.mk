6d29d22916b2b5a37fb0a326d2f1086cd5b6ebcd
synergydev <synergy@spentlemon.com>
2012-12-05 23:39:47 +0000

    core: CM_BUILD -> CFX_BUILD

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(CM_BUILD),)
-    all_product_configs := $(shell ls device/*/$(CM_BUILD)/cm.mk)
+  ifneq ($(CFX_BUILD),)
+    all_product_configs := $(shell ls device/*/$(CFX_BUILD)/cm.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # CM_BUILD
+  endif # CFX_BUILD
 endif
 
-ifeq ($(CM_BUILD),)
+ifeq ($(CFX_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
